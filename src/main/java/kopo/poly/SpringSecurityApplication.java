086d4269783039759d9be7e97b08d708b0fa420a
Song <mu9800mu12@naver.com>
2024-09-11 10:00:19 +0900

    스프링 시큐리티 실습 - 로그인 - 로그아웃 - 회원가입 - 스프링 벨리데이션

--- src/main/java/kopo/poly/SpringSecurityApplication.java
+++ src/main/java/kopo/poly/SpringSecurityApplication.java
@@ -3,7 +3,9 @@ package kopo.poly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @EnableJpaRepositories
 @SpringBootApplication
 public class SpringSecurityApplication {
